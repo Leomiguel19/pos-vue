18db2b31a14f3ded92aa972accb49f3fd1a4bd3a
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-15 20:22:39 -0400

    Install and config Formkit

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -9,5 +9,15 @@
         >Volver</Link>
 
         <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
+
+        <FormKit
+            type="form"
+        >
+            <FormKit
+                type="text"
+            >
+
+            </FormKit>
+        </FormKit>
     </div>
 </template> 
