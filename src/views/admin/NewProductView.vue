b536a8e5211b53b0014731b78bafe154d20e8d08
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-15 20:35:17 -0400

    Add Fields to New Product

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -12,12 +12,56 @@
 
         <FormKit
             type="form"
+            submit-label="Agregar Producto"
         >
             <FormKit
                 type="text"
-            >
+                label="Nombre"
+                name="nombre"
+                placeholder="Nombre de Producto"
+                validation="required"
+                :validation-messages="{required: 'El nombre del producto es obligatorio'}"
+            />
 
-            </FormKit>
+            <FormKit
+                type="file"
+                label="Imagen Producto"
+                name="image"
+                validation="required"
+                :validation-messages="{required: 'La imagen del producto es obligatoria'}"
+                accept=".jpg"
+            />
+
+            <FormKit
+                type="select"
+                label="Categoría"
+                name="category"
+                validation="required"
+                :validation-messages="{required: 'La categoría es obligatoria'}"
+                :options="[1,2,3]"
+            />
+
+            <FormKit
+                type="number"
+                label="Precio"
+                name="price"
+                placeholder="Precio de Producto"
+                validation="required"
+                :validation-messages="{required: 'El precio es obligatorio'}"
+                min="1"
+            />
+
+            
+            <FormKit
+                type="number"
+                label="Disponibles"
+                name="availability"
+                placeholder="Cantidad disponible"
+                validation="required"
+                :validation-messages="{required: 'La cantidad es obligatoria'}"
+                min="1"
+            />
+
         </FormKit>
     </div>
 </template> 
