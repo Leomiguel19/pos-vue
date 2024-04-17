cf447407de86e00dc1ab496c13be6ff9eb8cb1d8
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-16 21:22:19 -0400

    CSS to submit button

--- formkit.config.js
+++ formkit.config.js
@@ -7,20 +7,14 @@ const config = {
                 label: 'block mb-1 font-bold text-lg',
                 message: 'text-red-500 mb-5',
                 wrapper: 'space-y-2 mb-3',
-                input: 'w-full p-3 border-gray-300 rounded text-gray-700 placeholder-gray-400'
+                input: 'w-full p-3 border-gray-400 rounded text-gray-700 placeholder-gray-400'
             },
-            text: {
-
-            },
-            file:{
+            file: {
                 noFiles: 'block my-2',
                 fileItem: 'hidden'
             },
-            select:{
-
-            },
-            number: {
-
+            submit: {
+                input: '$reset bg-green-500 hover:bg-green-600 w-full p-2 font-bold uppercase disabled:opacity-50'
             }
         })
     }
