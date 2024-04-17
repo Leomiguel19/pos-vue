3bfae975f21fb8be5a9975f6b7a4b610301a8504
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-16 21:11:22 -0400

    CSS to inputs

--- formkit.config.js
+++ formkit.config.js
@@ -6,13 +6,15 @@ const config = {
             global: {
                 label: 'block mb-1 font-bold text-lg',
                 message: 'text-red-500 mb-5',
-                wrapper: 'space-y-2 mb-3'
+                wrapper: 'space-y-2 mb-3',
+                input: 'w-full p-3 border-gray-300 rounded text-gray-700 placeholder-gray-400'
             },
             text: {
 
             },
             file:{
-
+                noFiles: 'block my-2',
+                fileItem: 'hidden'
             },
             select:{
 
