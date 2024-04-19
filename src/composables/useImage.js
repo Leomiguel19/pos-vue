c49402f98097d3d23c36719f0604127a7d7ac3c2
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-18 21:12:23 -0400

    Upload images to Firebase Storage

--- src/composables/useImage.js
+++ src/composables/useImage.js
@@ -1,7 +1,30 @@
+import { uid } from 'uid'
+import { useFirebaseStorage } from 'vuefire'
+import { getDownloadURL, ref as storageRef, uploadBytesResumable } from 'firebase/storage'
+
 export default function useImage(){
 
+    const storage = useFirebaseStorage()
+
     const onFileChange = e => {
-        console.log(e.target.files[0])
+        const file = e.target.files[0]
+        const filename = uid() + '.jpg'
+        const sRef = storageRef(storage, '/products/' + filename)
+
+        // Sube el archivo 
+        const uploadTask = uploadBytesResumable(sRef, file)
+
+        uploadTask.on('state_changed', 
+            () => {},
+            (error) => console.log(error),
+            () => {
+                // upload is complete
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                        console.log(downloadURL)
+                    })
+            }
+        )
     }
 
     return {
