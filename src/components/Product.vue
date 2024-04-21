b29783c86e814e3701102dc53e4804c2770a360b
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-20 20:36:05 -0400

    Show Products

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import { formatCurrency } from '@/helpers';
+
     defineProps({
         product: {
             type: Object
@@ -7,8 +9,18 @@
 </script>
 
 <template>
-    <div>
-        <h1>{{ product.name }}</h1>
-    </div>
+    <li class="flex items-center space-x-6 border border-gray-200 p-6 bg-white shadow">
+        
+        <img 
+            :src="product.image" 
+            :alt="product.name"
+            class="h-24 w-24"
+        >
+        <div class="space-y-2">
+            <h3 class="text-gray-900">{{ product.name }}</h3>
+            <p class="font-extrabold">{{ formatCurrency(product.price) }}</p>
+            <p>{{ product.availability }} en existencia</p>
+        </div>
+    </li>
 </template>
 
