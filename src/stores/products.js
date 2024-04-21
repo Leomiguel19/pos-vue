02cef63e9bfd4666fc5e0e8b81403845cb154f50
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-20 20:28:26 -0400

    Get Firestore Products

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,7 +1,7 @@
 import { computed } from 'vue'
 import { defineStore } from 'pinia'
-import { useFirestore } from 'vuefire'
-import { collection, addDoc } from 'firebase/firestore'
+import { useFirestore, useCollection } from 'vuefire'
+import { collection, addDoc, where, query, limit, orderBy } from 'firebase/firestore'
 
 export const useProductsStore =  defineStore('products', () => {
     
@@ -12,6 +12,12 @@ export const useProductsStore =  defineStore('products', () => {
         { id: 2, name: 'Tenis' },
         { id: 3, name: 'Lentes' },
     ]
+
+    const q = query(
+        collection(db, 'products'),
+    )
+
+    const productsCollection = useCollection(q)
 
     async function createProduct(product){
         await addDoc(collection(db, 'products'), product)
@@ -29,6 +35,7 @@ export const useProductsStore =  defineStore('products', () => {
 
     return {
         createProduct,
-        categoryOptions 
+        categoryOptions,
+        productsCollection
     }
 })
